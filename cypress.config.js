fc59faf17f29cee03e40f57c7ed28b9c3d10603e
restufff <restuff666@gmail.com>
2024-06-08 16:22:07 +0700

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -5,26 +5,22 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "test/**/*.test.js",
+    supportFile: false,
   },
-  "viewportWidth": 1000,
-  "viewportHeight": 660,
-  "video": false,
-  "chromeWebSecurity": false,
-  "integrationFolder": "test",
-  "reporter": "mochawesome",
-  "testFiles": ["**/*.test.js"],
-  "pluginsFile": false,
-  "defaultCommandTimeout": 10000,
-  "reporterOptions": {
-      "overwrite": "false",
-      "reportPageTitle": "Automated Test Status",
-      "reportTitle": "Automated Test Reporting Dashboard",
-      "showPassed": "true",
-      "autoOpen": "false",
-      "charts": "true",
-      "code": "false"
+  viewportWidth: 1000,
+  viewportHeight: 660,
+  video: false,
+  chromeWebSecurity: false,
+  reporter: "mochawesome",
+  defaultCommandTimeout: 10000,
+  reporterOptions: {
+    overwrite: false,
+    reportPageTitle: "Automated Test Status",
+    reportTitle: "Automated Test Reporting Dashboard",
+    showPassed: true,
+    autoOpen: false,
+    charts: true,
+    code: false,
   },
-  "lint-staged": {
-    "*.{js,md}": "prettier --write --ignore-unknown"
-  }
 });
