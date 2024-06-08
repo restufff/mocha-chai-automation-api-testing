7ad9effc2cfd35c7bee071deba334ff1d5202374
restufff <restuff666@gmail.com>
2024-06-08 18:30:05 +0700

    fix bangat

--- test/scenarios/satu.test.js
+++ test/scenarios/satu.test.js
@@ -1,39 +1,40 @@
-import axios from 'axios';
-import { assert, expect } from "chai";
-import dotenv from 'dotenv';
+describe('As a user I want to test Gorest Public API', () => {
+  const API_BASE_URL = Cypress.env('BASEURL');
 
-dotenv.config();
+  it('should get only 10 todos', () => {
+    cy.request(`https://gorest.co.in/public/v2/todos`).then((response) => {
+      // Verify the status code
+      expect(response.status).to.eq(200);
 
-describe('As an user I want to test Gorest Public API', () => {
-  const API_BASE_URL = process.env.BASEURL;
+      // Verify the length of the array is 10
+      expect(response.body).to.have.lengthOf(10);
 
-  it('should get only 10 todos', async () => {
-    const response = await axios.get(`${API_BASE_URL}/todos`);
-    // assert.equal(response.status, 200);
-    // assert(Array.isArray(response.data));
-    // expect(response.data).to.have.lengthOf(10);
-
-    // response.data.forEach(todo => {
-    //   assert.strictEqual(typeof todo.id, 'number');
-    //   assert.strictEqual(typeof todo.user_id, 'number');
-    //   assert.strictEqual(typeof todo.title, 'string');
-    //   expect(response.data[0]).to.have.all.keys("id", "user_id", "title", "due_on", "status");
-    // });
+      // Verify each todo item has correct data types and required keys
+      response.body.forEach(todo => {
+        expect(todo).to.have.all.keys('id', 'user_id', 'title', 'due_on', 'status');
+        expect(todo.id).to.be.a('number');
+        expect(todo.user_id).to.be.a('number');
+        expect(todo.title).to.be.a('string');
+        expect(todo.due_on).to.be.a('string'); // assuming due_on is a string
+        expect(todo.status).to.be.a('string');
+      });
+    });
   });
 
-  it('should get users with status inactive and validate data', async () => {
-    const response = await axios.get(`${API_BASE_URL}/users?status=inactive`);
-    // assert.strictEqual(response.status, 200);
-    // assert(Array.isArray(response.data));
+  it('should get users with status inactive and validate data', () => {
+    cy.request(`https://gorest.co.in/public/v2/users?status=inactive`).then((response) => {
+      // Verify the status code
+      expect(response.status).to.eq(200);
 
-    // response.data.forEach(user => {
-    //   assert.strictEqual(typeof user.id, 'number');
-    //   assert.strictEqual(typeof user.name, 'string');
-    //   assert.strictEqual(typeof user.email, 'string');
-    //   assert.strictEqual(typeof user.email, 'string');
-    //   assert.strictEqual(typeof user.status, 'string');
-    //   expect(response.data[0]).to.have.all.keys("id", "name", "email", "gender", "status");
-    //   expect(user.status).to.equal('inactive');
-    // });
+      // Verify each user item has correct data types and required keys
+      response.body.forEach(user => {
+        expect(user).to.have.all.keys('id', 'name', 'email', 'gender', 'status');
+        expect(user.id).to.be.a('number');
+        expect(user.name).to.be.a('string');
+        expect(user.email).to.be.a('string');
+        expect(user.gender).to.be.a('string'); // assuming gender is a string
+        expect(user.status).to.equal('inactive');
+      });
+    });
   });
 });
