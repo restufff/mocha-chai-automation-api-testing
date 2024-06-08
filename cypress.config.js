6a2b35817adef9d28d136b61d642745aabe49dc2
restufff <restuff666@gmail.com>
2024-06-08 16:18:52 +0700

    get rid of cypress.json

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,25 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  "viewportWidth": 1000,
+  "viewportHeight": 660,
+  "video": false,
+  "chromeWebSecurity": false,
+  "integrationFolder": "test",
+  "reporter": "mochawesome",
+  "testFiles": ["**/*.test.js"],
+  "pluginsFile": false,
+  "defaultCommandTimeout": 10000,
+  "reporterOptions": {
+      "overwrite": "false",
+      "reportPageTitle": "Automated Test Status",
+      "reportTitle": "Automated Test Reporting Dashboard",
+      "showPassed": "true",
+      "autoOpen": "false",
+      "charts": "true",
+      "code": "false"
+  },
+  "lint-staged": {
+    "*.{js,md}": "prettier --write --ignore-unknown"
+  }
 });
